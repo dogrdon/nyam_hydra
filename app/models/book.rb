b8745c74759713f96d648916fe7894d03552d9e1
droquo <drew.s.gordon@gmail.com>
2014-06-17 03:50:08 +0000

    add pages to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ActiveFedora::Base
   
   has_metadata 'descMetadata', type: BookMetadata
   
+  #associate with pages
+  has_many :pages, :property=> :is_part_of
+  
   has_attributes :title, datastream: 'descMetadata', multiple: false
   has_attributes :author, datastream: 'descMetadata', multiple: true
   has_attributes :published, datastream: 'descMetadata', multiple: false
