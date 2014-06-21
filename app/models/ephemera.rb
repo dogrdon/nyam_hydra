9392ca0657871cc3480f6e604f1e654155f5f3c2
droquo <drew.s.gordon@gmail.com>
2014-06-20 22:34:03 -0400

    fix multiples

--- app/models/ephemera.rb
+++ app/models/ephemera.rb
@@ -15,7 +15,7 @@ class Ephemera < ActiveFedora::Base
   has_attributes :format, datastream: 'descMetadata', multiple: true
   has_attributes :source, datastream: 'descMetadata', multiple: false
   has_attributes :relation, datastream: 'descMetadata', multiple: false
-  has_attributes :coverage, datastream: 'descMetadata', multiple: false
+  has_attributes :coverage, datastream: 'descMetadata', multiple: true
   has_attributes :rights, datastream: 'descMetadata', multiple: true
   has_attributes :contributors, datastream: 'descMetadata', multiple: true
   has_attributes :text, datastream: 'descMetadata', multiple: false
@@ -23,12 +23,12 @@ class Ephemera < ActiveFedora::Base
   has_attributes :related_to, datastream: 'descMetadata', multiple: false
   has_attributes :technique, datastream: 'descMetadata', multiple: true
   has_attributes :date_created, datastream: 'descMetadata', multiple: false
-  has_attributes :repository, datastream: 'descMetadata', multiple: false
-  has_attributes :doc_type, datastream: 'descMetadata', multiple: false
+  has_attributes :repository, datastream: 'descMetadata', multiple: true
+  has_attributes :doc_type, datastream: 'descMetadata', multiple: true
   has_attributes :height, datastream: 'descMetadata', multiple: false
   has_attributes :width, datastream: 'descMetadata', multiple: false
   has_attributes :language, datastream: 'descMetadata', multiple: true
-  has_attributes :audience, datastream: 'descMetadata', multiple: false
+  has_attributes :audience, datastream: 'descMetadata', multiple: true
   has_attributes :tag, datastream: 'descMetadata', multiple: true
 
   
