9a1f88ff999a7d1b2b0b48ded6cb681d35b03d88
droquo <drew.s.gordon@gmail.com>
2014-07-12 17:42:15 -0400

    ephemerapage metadata

--- app/models/datastreams/ephemerapage_metadata.rb
+++ app/models/datastreams/ephemerapage_metadata.rb
@@ -4,6 +4,10 @@ class Datastreams::EphemeraPageMetadata < ActiveFedora::OmDatastream
     t.root(path: "fields")
     t.number(index_as: :stored_searchable, type: :integer)
     t.text(index_as: :stored_searchable)
+    t.cdmid
+    t.child_of
+    t.object_type
+    t.filename
   end
   
   def self.xml_template
--- app/models/ephemerapage.rb
+++ app/models/ephemerapage.rb
@@ -6,9 +6,12 @@ class EphemeraPage < ActiveFedora::Base
   belongs_to :ephemera, :property=> :is_part_of
   
   
-  has_attributes :number, datastream: 'descMetadata', multiple: false
+  has_attributes :cdmid, datastream: 'descMetadata', multiple: false
   has_attributes :text, datastream: 'descMetadata', multiple: false
   has_attributes :notes, datastream: 'descMetadata', multiple: false
+  has_attributes :child_of, datastream: 'descMetadata', multiple: false
+  has_attributes :object_type, datastream: 'descMetadata', multiple: false
+  has_attributes :filename, datastream: 'descMetadata', multiple: false
   
   
   has_file_datastream "pageContent"
